a29678530ceadaaafda9bf78db74bbe5c2fc9b6f
Pedro Augusto Ribeiro Mangoni <167664560+pedrormamgoni@users.noreply.github.com>
2024-11-20 12:01:47 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Saude-e-bem-estar',
+  webDir: 'www'
+};
+
+export default config;
